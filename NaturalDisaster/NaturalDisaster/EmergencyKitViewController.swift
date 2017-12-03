da4c387e695846472bcbd91daf8ccfac15adf00f
Neha <nehabashyam@gmail.com>
2017-12-02 17:09:31 -0800

    redid emergencykit

---
+++ NaturalDisaster/NaturalDisaster/EmergencyKitViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EmergencyKitViewController.swift
+//  NaturalDisaster
+//
+//  Created by EmilyC on 12/2/17.
+//  Copyright © 2017 Emily Chin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EmergencyKitViewController: UIViewController {
+    
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
